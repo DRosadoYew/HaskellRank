8da015423383e9e5d6b84e64812bba965e960d58
Daniel Rosado <daniel.rosado@collab.pt>
2015-02-16 01:30:45 +0100

    Solution to 'Hello World N Times'.

---
+++ HelloWorldNTimes/HelloWorldNTimes.hs
@@ -0,0 +1,8 @@
+hello_worlds 0 = return ()
+hello_worlds n = do
+               putStrLn "Hello World"
+               hello_worlds (n - 1)
+
+main = do
+     n <- readLn :: IO Int
+     hello_worlds n
